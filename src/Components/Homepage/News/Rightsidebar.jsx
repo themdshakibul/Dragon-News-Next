db2ffcfb22b62eecbe7f2d6d9c14e33f4b9de138
themdshakibul <mdshakibul88240@gmail.com>
2026-04-27 00:51:11 +0600

    loging width google

--- src/Components/Homepage/News/Rightsidebar.jsx
+++ src/Components/Homepage/News/Rightsidebar.jsx
@@ -1,10 +1,22 @@
+"use client";
+
+import { authClient } from "@/lib/auth-client";
 import { FaGithub, FaGoogle } from "react-icons/fa";
 
 const Rightsidebar = () => {
+  const handelGoogleSignin = async () => {
+    const data = await authClient.signIn.social({
+      provider: "google",
+    });
+  };
+
   return (
     <section>
       <div className="flex flex-col gap-3 mt-5">
-        <button className="btn border-2 border-blue-500 text-blue-500">
+        <button
+          onClick={handelGoogleSignin}
+          className="btn border-2 border-blue-500 text-blue-500"
+        >
           <FaGoogle /> Loging width Google
         </button>
         <button className="btn border-2">
